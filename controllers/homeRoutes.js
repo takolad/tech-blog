8828f72e17e3506d4e2104117fb65b3db028c23d
John Hinojosa <hinojohn@ymail.com>
2021-04-27 23:50:41 -0500

    updated route links in controller index.js file, added coding for authorization and handlebars templates

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { User, Blog } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      attributes: { exclude: ['id'] },
+      order: [
+        ['date_created', 'DSC'],
+        ['id', 'ASC'],
+      ]
+    });
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
